7365db36aab1a70c7de69624a63db82f908268a9
Dibyashwor Hamal <hamaldivyashwor2057@gmail.com>
2025-09-13 08:19:27 +0545

    feat: Build static UI for terminal with header, history, and input components

---
+++ src/components/Input.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Input = () => {
+  // We will add state and handlers here on Day 6.
+  // For now, it's just the visual structure.
+  return (
+    <div>
+      <span style={{ color: '#4AF626' }}>&gt; </span>
+      {/* The blinking cursor will be added later with CSS */}
+    </div>
+  );
+};
+
+export default Input;
